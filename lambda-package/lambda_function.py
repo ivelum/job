b8bdcda5609410c8e184f008029f7cecc9de941d
Sergey Fursov <geyser85@gmail.com>
2021-08-24 17:29:01 +0300

    add API gateway for calling lambda function

--- lambda-package/lambda_function.py
+++ lambda-package/lambda_function.py
@@ -1,6 +1,8 @@
 import json
 import os
+import sys
 from datetime import datetime
+from json import JSONDecodeError
 
 import gspread
 
@@ -9,8 +11,26 @@ def lambda_handler(event, context):
     info = json.loads(os.environ['GOOGLE_API_SERVICE_ACCOUNT_INFO'])
     spreadsheet_id = os.environ['GOOGLE_SPREADSHEET_ID']
 
-    job = event.pop('job')
-    event['ts'] = datetime.now().isoformat()
+    try:
+        event_body = json.loads(event['body'])
+        assert isinstance(event_body, dict)
+    except (KeyError, JSONDecodeError, AssertionError):
+        sys.stdout.write(str(event))
+        return {
+            'status': 'error',
+            'message': 'Wrong input: malformed body.',
+        }
+
+    try:
+        job = event_body.pop('job')
+    except KeyError:
+        sys.stdout.write(str(event))
+        return {
+            'status': 'error',
+            'message': 'Wrong input: missing "job" param.',
+        }
+
+    event_body['ts'] = datetime.now().isoformat()
 
     gc = gspread.service_account_from_dict(info)
     wb = gc.open_by_key(spreadsheet_id)
@@ -19,11 +39,12 @@ def lambda_handler(event, context):
     except gspread.exceptions.WorksheetNotFound:
         ws = wb.add_worksheet(job, rows=1000, cols=100)
     fields = ws.row_values(1)
+
     values = []
     for field in fields:
-        values.append(event.get(field, ''))
+        values.append(event_body.get(field, ''))
 
-    missing_values = {k: v for k, v in event.items() if k not in fields}
+    missing_values = {k: v for k, v in event_body.items() if k not in fields}
     new_row_num = len(ws.col_values(1)) + 1
     ws.update(f'A{new_row_num}', [values])
     if missing_values:
