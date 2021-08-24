3418a1028282710e931f251a7427eb5ddffbaaf1
Sergey Fursov <geyser85@gmail.com>
2021-08-24 15:41:05 +0300

    switch python form to self-written

--- src/Jobs.js
+++ src/Jobs.js
@@ -3,12 +3,10 @@ const Jobs = {
   php: {
     title: 'Full-stack разработчик (PHP + frontend)',
     url: '/php/',
-    formId: 'd/e/1FAIpQLSf-2NaYgI9rAxhUQaRx0rnAFeum8eAvOprX08cF-PaM_qS5bw',
   },
   python: {
     title: 'Full-stack разработчик (Python/Django + frontend)',
     url: '/python/',
-    formId: 'd/e/1FAIpQLSeOzVt6OqfzIwJ11DIN7BUp65A3yPh98RkyJ_9AmHoBX0PAjA',
   },
 };
 
--- src/pages/python/form.jsx
+++ src/pages/python/form.jsx
@@ -1,8 +1,14 @@
+import omit from 'lodash/omit';
 import React from 'react';
 
 import Jobs from '../../Jobs';
-import GoogleForm from '@components/GoogleForm';
+import ApplyForm, { allExperienceTypes } from '@components/ApplyForm';
 
 export default function PythonForm() {
-  return <GoogleForm job={Jobs.python} />;
+  return (
+    <ApplyForm
+      job={Jobs.python}
+      experienceTypes={omit(allExperienceTypes, ['php'])}
+    />
+  );
 }
