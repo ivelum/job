714f3f750078c5cc7ef6f4c524e114c00783afb8
Denis Stebunov <denis.stebunov@ivelum.com>
2021-09-01 10:03:41 +0300

    fix jobs order on the home page

--- src/Jobs.js
+++ src/Jobs.js
@@ -1,18 +1,18 @@
 // Jobs. Steve Jobs.
 const Jobs = {
-  php: {
-    name: 'PHP',
-    title: 'PHP разработчик',
-    subTitle: 'Full-stack',
-    description: 'Full-stack разработчик (PHP + frontend)',
-    url: '/php/',
-  },
   python: {
     name: 'Python',
     title: 'Python/Django разработчик',
     subTitle: 'Full-stack',
     description: 'Full-stack разработчик (Python/Django + frontend)',
     url: '/python/',
+  },
+  php: {
+    name: 'PHP',
+    title: 'PHP разработчик',
+    subTitle: 'Full-stack',
+    description: 'Full-stack разработчик (PHP + frontend)',
+    url: '/php/',
   },
 };
 
