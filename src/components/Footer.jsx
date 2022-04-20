3eda471c6a385a693dc1758f24ce93c7790a4858
Anton Skorodumov <info@kreades.ru>
2022-04-20 12:23:44 +0700

    add YouTube button; fix current year in footer;

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,11 +1,22 @@
 import React from 'react';
 
+import ExternalLink from '@/components/ExternalLink';
+import SvgImage from '@/components/SvgImage';
+
 import * as styles from './Footer.module.scss';
 
 export default function Footer() {
   return (
     <div className={styles.footer}>
-      © ivelum 2003 – 2022
+      <ExternalLink
+        href="https://www.youtube.com/channel/UChgBoJzbKwLzzaU2nMNWUDw/"
+        className={styles.youtube}
+      >
+        <SvgImage name="ico-youtube" />Наш YouTube
+      </ExternalLink>
+      <div className={styles.copyright}>
+        © ivelum 2003 – {new Date().getFullYear()}
+      </div>
     </div>
   );
 }
