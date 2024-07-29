cfa0ea5baba2c0e86d905d59c1f0e3ab991746e8
Denis Stebunov <denis.stebunov@ivelum.com>
2024-07-29 18:36:48 +0200

    close Python position

--- src/Jobs.js
+++ src/Jobs.js
@@ -1,19 +1,19 @@
 // Jobs. Steve Jobs.
 const Jobs = {
-  python: {
-    name: 'Python',
-    title: 'Python/Django разработчик',
-    subTitle: 'Full-stack',
-    description: 'Full-stack разработчик (Python/Django + frontend)',
-    url: '/python/',
-    active: true,
-  },
   marketing: {
     name: 'Marketing',
     title: 'Head of Marketing',
     description: 'Product Marketing, Content, Digital, SEO, SMM',
     url: '/marketing/',
     active: true,
+  },
+  python: {
+    name: 'Python',
+    title: 'Python/Django разработчик',
+    subTitle: 'Full-stack',
+    description: 'Full-stack разработчик (Python/Django + frontend)',
+    url: '/python/',
+    active: false,
   },
   ux: {
     name: 'UX',
