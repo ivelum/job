315e20642d3940ee823573e7c1a646996354852b
Andrey Maslov <greyzmeem@gmail.com>
2026-02-02 14:34:23 +0200

    Deploy: fix variable update in for-loop

--- deploy/cli/deploy.py
+++ deploy/cli/deploy.py
@@ -6,7 +6,7 @@ import click
 
 from deploy.aws import aws, resource_details
 from deploy.settings import BASE_DIR, LAMBDA_FUNCTIONS, PROJECT_NAME
-from deploy.utils import run, timing
+from deploy.utils import echo, run, timing
 
 
 @click.group()
@@ -56,6 +56,7 @@ def deploy_lambda():
     code_archive_path = f'fileb://{package_path}/{code_archive_name}'
     for func_name in LAMBDA_FUNCTIONS:
         func_id = resource_ids[func_name]
+        echo(f'Updating function code: {func_name}')
         aws(
             f'lambda update-function-code '
             f'--function-name {func_id} '
@@ -72,12 +73,14 @@ def deploy_lambda():
     updated_functions: set[str] = set()
     while in_progress_functions:
         time.sleep(1)
-        for func_name in in_progress_functions:
+        for func_name in set(in_progress_functions):
             status = get_function_config(func_name)['LastUpdateStatus']
+            echo(f'Function {func_name} status: {status}')
             match status:
                 case 'InProgress':
                     continue
                 case 'Failed':
+                    echo(f'Function {func_name} failed to update')
                     in_progress_functions.remove(func_name)
                     failed_functions.add(func_name)
                 case _:
@@ -90,6 +93,7 @@ def deploy_lambda():
     slack_bot_token = os.environ['SLACK_BOT_TOKEN']
     pipedrive_token = os.environ['PIPEDRIVE_TOKEN']
     for func_name in updated_functions:
+        echo(f'Updating function configuration: {func_name}')
         func_id = resource_ids[func_name]
         aws(
             f'lambda update-function-configuration '
